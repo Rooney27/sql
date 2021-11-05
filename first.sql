4d4df58f41a75107e31922d7243bbbc762ddca82
Rooney27 <vasilewskij.fil@gmail.com>
2021-11-05 06:00:02 +0300

    Initial commit

---
+++ first.sql
@@ -0,0 +1,8 @@
+﻿SELECT client_number as client, 
+                                SUM(outcome = "win") as win, 
+                                SUM(outcome = "lose") AS lose 
+                        FROM bid
+                        INNER JOIN event_value
+                        ON bid.play_id = event_value.play_id
+                        WHERE bid.coefficient = event_value.value
+                        GROUP BY client_number
---
+++ second.sql
@@ -0,0 +1,2 @@
+﻿SELECT CONCAT(home_team,"-",away_team) AS A, COUNT(*)
+FROM event_entity GROUP BY A ORDER BY COUNT(*)
